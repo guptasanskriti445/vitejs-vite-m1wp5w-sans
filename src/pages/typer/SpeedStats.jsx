9c97642ff70bc8af568a6e42aa67768c4746d794
guptasanskriti445 <guptasanskriti445@gmail.com>
2024-09-03 16:09:07 +0530

    add changes

--- src/pages/typer/SpeedStats.jsx
+++ src/pages/typer/SpeedStats.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SpeedStats = ({ wpm, accuracy }) => {
   return (
-    <div className="speed-stats">
+    <div className="speed-stats"> 
       <p>Words per minute: {wpm}</p>
       <p>Accuracy: {accuracy}%</p>
     </div>
